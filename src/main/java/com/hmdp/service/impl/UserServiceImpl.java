0a44b71abab7357557cb24b60abf524166e30e5c
aaa <2877809789@qq.com>
2026-08-01 16:28:57 +0800

    点评-短信登录校验业务代码开发

---
+++ src/main/java/com/hmdp/service/impl/UserServiceImpl.java
@@ -0,0 +1,127 @@
+package com.hmdp.service.impl;
+
+import cn.hutool.core.bean.BeanUtil;
+import cn.hutool.core.bean.copier.CopyOptions;
+import cn.hutool.core.util.RandomUtil;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.hmdp.dto.LoginFormDTO;
+import com.hmdp.dto.Result;
+import com.hmdp.dto.UserDTO;
+import com.hmdp.entity.User;
+import com.hmdp.mapper.UserMapper;
+import com.hmdp.service.IUserService;
+import com.hmdp.utils.RedisConstants;
+import com.hmdp.utils.RegexUtils;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpSession;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+import java.util.concurrent.TimeUnit;
+
+import static com.hmdp.utils.SystemConstants.USER_NICK_NAME_PREFIX;
+
+/**
+ * <p>
+ * 服务实现类
+ * </p>
+ *
+ * @author 虎哥
+ * @since 2021-12-22
+ */
+@Service
+@Slf4j
+public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements IUserService {
+
+    @Resource
+    private StringRedisTemplate stringRedisTemplate;
+
+    /**
+     * 发送验证码
+     * @param phone 手机号
+     * @return 发送结果
+     */
+    @Override
+    public Result sendCode(String phone, HttpSession session) {
+        //1. 验证手机号
+        if (RegexUtils.isPhoneInvalid(phone)) {
+            //2.如果不符合，返回错误信息
+            return Result.fail("手机号格式错误!");
+        }
+        //3.符合，生成验证码
+        String code = RandomUtil.randomNumbers(6);
+
+        //4.保存验证码到redis
+        stringRedisTemplate.opsForValue().set(RedisConstants.LOGIN_CODE_KEY + phone, code,2, TimeUnit.MINUTES);
+
+        //5.发送验证码
+        log.debug("发送短信验证码成功，验证码：{}", code);
+
+        //6.返回ok
+        return Result.ok();
+    }
+
+    /**
+     * 登录功能
+     * @param loginForm 登录参数，包含手机号、验证码；或者手机号、密码
+     */
+    @Override
+    public Result login(LoginFormDTO loginForm, HttpSession session) {
+        //1.校验手机号
+        String phone = loginForm.getPhone();
+        if (RegexUtils.isPhoneInvalid(phone)) {
+            //2.如果不符合，返回错误信息
+            return Result.fail("手机号格式错误!");
+        }
+
+        //3.从redis获取验证码并校验
+        Object cacheCode = stringRedisTemplate.opsForValue().get(RedisConstants.LOGIN_CODE_KEY + phone);
+        String code = loginForm.getCode();
+        if (cacheCode == null || !cacheCode.toString().equals(code)) {
+            //不一致，返回错误信息
+            return Result.fail("验证码错误!");
+        }
+
+        //4.一致，查询用户 select * from tb_user where phone = ?
+        User user = query().eq("phone", phone).one();
+
+        //5.判断用户是否存在
+        if (user == null) {
+            //6.不存在，创建新用户并保存
+            user = createUserWithPhone(phone);
+        }
+
+        //7.保存用户信息到redis
+        //7.1随机生成token，作为登录令牌
+        String token = UUID.randomUUID().toString();
+        //7.2将User对象转为Hash存储
+        UserDTO userDTO = BeanUtil.copyProperties(user, UserDTO.class);
+        Map<String, Object> userMap = BeanUtil.beanToMap(userDTO,new HashMap<>(),
+                CopyOptions.create()
+                        .setIgnoreNullValue(true)
+                        .setFieldValueEditor((fieldName,fieldValue)->fieldValue.toString()));//将UserDTO对象转为Map
+        //7.3存储
+        stringRedisTemplate.opsForHash().putAll(RedisConstants.LOGIN_USER_KEY + token, userMap);
+        //7.4设置token有效时间
+        stringRedisTemplate.expire(RedisConstants.LOGIN_USER_KEY + token, RedisConstants.LOGIN_USER_TTL, TimeUnit.MINUTES);
+
+        //8.返回token
+        return Result.ok(token);
+    }
+
+    private User createUserWithPhone(String phone) {
+        //1.创建用户对象
+        User user = new User();
+        user.setPhone(phone);
+        user.setNickName(USER_NICK_NAME_PREFIX+ RandomUtil.randomString(10));
+        //2.保存用户
+        save(user);
+        return user;
+    }
+
+}
